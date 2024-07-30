544d367c10c6809b618a5b10c4bb2d48561ec224
dokonosuke <133506040+dokonosuke@users.noreply.github.com>
2024-07-30 21:28:16 +0900

    Initial Commit

---
+++ Calcu1at0r/Calcu1at0rApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calcu1at0rApp.swift
+//  Calcu1at0r
+//
+//  Created by 獨鈷青空 on 2024/07/30.
+//
+
+import SwiftUI
+
+@main
+struct Calcu1at0rApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calcu1at0r/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calcu1at0r
+//
+//  Created by 獨鈷青空 on 2024/07/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
